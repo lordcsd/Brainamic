dd74549a559afe753fa4ae592fbcb8f441fc419d
lordcsd <dimgbachinonso@gmail.com>
2022-07-05 00:22:44 +0100

    fixing heroku issue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,15 +9,15 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v1');
 
   const options = new DocumentBuilder()
-  .setTitle("rocket global Exchange Market")
-  .setDescription("rocket global Exchange Market")
-  .setVersion("1.0")
-  .addBearerAuth()
-  .build();
+    .setTitle('rocket global Exchange Market')
+    .setDescription('rocket global Exchange Market')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
-const document = SwaggerModule.createDocument(app, options);
-SwaggerModule.setup("api-doc", app, document);
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-doc', app, document);
 
-  await app.listen(+process.env.PORT);
+  await app.listen(+process.env.PORT || 3012);
 }
 bootstrap();
